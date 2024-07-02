e2e522c66bf3f45d60118b481d94c6c29d23d9ed
MarcosPines <149545283+Marcos-Pines1@users.noreply.github.com>
2024-07-02 20:31:06 -0300

    links de los portafolios

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+/*===================== LOADING ========================*/
+
+const loadingElement = document.querySelector("[data-loading]");
+
+window.addEventListener("load", function () {
+  loadingElement.classList.add("loaded");
+});
+
 /*===================== NAV MENU ========================*/
 
 const navMenu = document.getElementById("menu"),
